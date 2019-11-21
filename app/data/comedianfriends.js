7170b690d35c4d66450cd7530c08660d7177889c
liamwynne <liamdwynne5753@gmail.com>
2019-11-21 10:00:35 -0500

    fill in comedian.js

---
+++ app/data/comedianfriends.js
@@ -0,0 +1,43 @@
+// Comedians list
+var comedians = [{
+
+    name: "Bert Keischer",
+    photo: "insert link",
+    scores: [1, 4, 5, 3, 2, 5, 4, 4, 5, 1]
+},
+{
+    name:"Nikki Glaser",
+    photo: "insert link",
+    scores:[4, 5, 3, 3, 1, 2, 1, 1, 3, 5]
+},
+{
+    name: "Eddie Murphy",
+    photo: "insert link",
+    scores: [4, 4, 4, 4, 4, 1, 5, 2, 4, 2]
+},
+{
+    name: "Kevin Hart",
+    photo: "insert link",
+    scores: [5, 5, 2, 2, 5, 4, 5, 5, 1, 2]
+},
+{
+    name: "Sarah Silverman",
+    photo: "insert link",
+    scores: [3, 3, 3, 5, 5, 2, 4, 1, 5, 5]
+},
+{
+    name: "Bill Burr",
+    photo: "insert link",
+    scores: [4, 3, 2, 1, 1, 5, 2, 3, 5, 5]
+},
+{
+    name: "Amy Schumer",
+    photo: "insert link",
+    scores: [5, 5, 5, 5, 5, 3, 1, 5, 5, 4]
+},
+{
+    name: "Russell Howard",
+    photo: "insert link",
+    scores: [3, 3, 3, 4, 5, 3, 4, 3, 4, 3]
+},
+]
