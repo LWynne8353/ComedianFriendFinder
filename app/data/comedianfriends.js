992ec84f4e000e61db94250b299c68ad6868e424
liamwynne <liamdwynne5753@gmail.com>
2019-12-06 15:13:51 -0500

    added photos to comedianfinder.js

--- app/data/comedianfriends.js
+++ app/data/comedianfriends.js
@@ -2,42 +2,42 @@
 var comedians = [{
 
     name: "Bert Keischer",
-    photo: "insert link",
+    photo: "assets/images/BertKreischer-374x345.jpg",
     scores: [1, 4, 5, 3, 2, 5, 4, 4, 5, 1]
 },
 {
     name:"Nikki Glaser",
-    photo: "insert link",
+    photo: "assets/images/Nikki Glaser.jpg",
     scores:[4, 5, 3, 3, 1, 2, 1, 1, 3, 5]
 },
 {
     name: "Eddie Murphy",
-    photo: "insert link",
+    photo: "assets/images/EddieMurphy.jpg",
     scores: [4, 4, 4, 4, 4, 1, 5, 2, 4, 2]
 },
 {
     name: "Kevin Hart",
-    photo: "insert link",
+    photo: "assets/images/KevinHart.jpg",
     scores: [5, 5, 2, 2, 5, 4, 5, 5, 1, 2]
 },
 {
     name: "Sarah Silverman",
-    photo: "insert link",
+    photo: "assets/images/Sarah Silverman.jpg",
     scores: [3, 3, 3, 5, 5, 2, 4, 1, 5, 5]
 },
 {
     name: "Bill Burr",
-    photo: "insert link",
+    photo: "assets/images/Bill Burr.jpg",
     scores: [4, 3, 2, 1, 1, 5, 2, 3, 5, 5]
 },
 {
     name: "Amy Schumer",
-    photo: "insert link",
+    photo: "assets/images/AmySchumer.jpg",
     scores: [5, 5, 5, 5, 5, 3, 1, 5, 5, 4]
 },
 {
     name: "Russell Howard",
-    photo: "insert link",
+    photo: "assets/images/RussellHoward.jpg",
     scores: [3, 3, 3, 4, 5, 3, 4, 3, 4, 3]
 },
 ]
