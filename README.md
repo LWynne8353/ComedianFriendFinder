8d29bc364a6954233c844b0cead0d2cfe58d41b8
liamwynne <liamdwynne5753@gmail.com>
2020-01-05 21:25:17 -0500

    added small readme

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ComedianFriendFinder
+
+This survey is to help you find the comedian who will most likely hang out with you. 
--- app/data/friends.js
+++ app/data/friends.js
@@ -2,42 +2,42 @@
 var comedians = [{
 
     name: "Bert Keischer",
-    photo: "assets/images/BertKreischer-374x345.jpg",
-    scores: [1, 4, 5, 3, 2, 5, 4, 4, 5, 1]
+    photo: "assets\images\BertKreischer-374x345.jpg",
+    scores: ["1","4","5", "3", "2", "5", "4", "4", "5", "1"]
 },
 {
     name:"Nikki Glaser",
-    photo: "assets/images/Nikki Glaser.jpg",
-    scores:[4, 5, 3, 3, 1, 2, 1, 1, 3, 5]
+    photo: "assets\images\Nikki Glaser.jpg",
+    scores:["4", "5", "3", "3", "1", "2", "1", "1", "3", "5"]
 },
 {
     name: "Eddie Murphy",
-    photo: "assets/images/EddieMurphy.jpg",
-    scores: [4, 4, 4, 4, 4, 1, 5, 2, 4, 2]
+    photo: "assets\images\EddieMurphy.jpg",
+    scores: ["4", "4", "4", "4", "4", "1", "5", "2", "4", "2"]
 },
 {
     name: "Kevin Hart",
-    photo: "assets/images/KevinHart.jpg",
-    scores: [5, 5, 2, 2, 5, 4, 5, 5, 1, 2]
+    photo: "assets\images\KevinHart.jpg",
+    scores: ["5", "5", "2", "2", "5", "4", "5", "5", "1","2"]
 },
 {
     name: "Sarah Silverman",
-    photo: "assets/images/Sarah Silverman.jpg",
-    scores: [3, 3, 3, 5, 5, 2, 4, 1, 5, 5]
+    photo: "assets\images\Sarah Silverman.jpg",
+    scores: ["3", "3", "3", "5", "5", "2", "4", "1", "5", "5"]
 },
 {
     name: "Bill Burr",
-    photo: "assets/images/Bill Burr.jpg",
-    scores: [4, 3, 2, 1, 1, 5, 2, 3, 5, 5]
+    photo: "assets\images\Bill Burr.jpg",
+    scores: ["4", "3", "2", "1", "1", "5", "2", "3", "5", "5"]
 },
 {
     name: "Amy Schumer",
-    photo: "assets/images/AmySchumer.jpg",
-    scores: [5, 5, 5, 5, 5, 3, 1, 5, 5, 4]
+    photo: "assets\images\AmySchumer.jpg",
+    scores: ["5", "5", "5", "5", "5", "3", "1", "5", "5", "4"]
 },
 {
     name: "Russell Howard",
-    photo: "assets/images/RussellHoward.jpg",
+    photo: "assets\images\RussellHoward.jpg",
     scores: ["3", "3", "3", "4", "5", "3", "4", "3", "4", "3"]
 },
 ];
