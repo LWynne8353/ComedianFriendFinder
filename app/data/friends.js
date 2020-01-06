6a07b009a6342341eb9a00de06748d89c3ddc607
liamwynne <liamdwynne5753@gmail.com>
2020-01-06 15:16:23 -0500

    fix

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,22 +2,22 @@
 var comedians = [{
 
     name: "Bert Keischer",
-    photo: "assets\images\BertKreischer-374x345.jpg",
+    photo: "assets/images/BertKreischer-374x345.jpg",
     scores: ["1","4","5", "3", "2", "5", "4", "4", "5", "1"]
 },
 {
     name:"Nikki Glaser",
-    photo: "assets\images\Nikki Glaser.jpg",
+    photo: "assets/images/Nikki Glaser.jpg",
     scores:["4", "5", "3", "3", "1", "2", "1", "1", "3", "5"]
 },
 {
     name: "Eddie Murphy",
-    photo: "assets\images\EddieMurphy.jpg",
+    photo: "assets/images/EddieMurphy.jpg",
     scores: ["4", "4", "4", "4", "4", "1", "5", "2", "4", "2"]
 },
 {
     name: "Kevin Hart",
-    photo: "assets\images\KevinHart.jpg",
+    photo: "assets/images/KevinHart.jpg",
     scores: ["5", "5", "2", "2", "5", "4", "5", "5", "1","2"]
 },
 {
